544b6cf268af8b6488483062d2d97445ce6684af
Chronos <123748825+dtyafa@users.noreply.github.com>
2023-10-13 18:48:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def process_start_command(message: types.Message):
     markup.add(types.KeyboardButton('/Розклад'))
     markup.add(types.KeyboardButton('/Переглянути ДЗ'))
     markup.add(types.KeyboardButton('/Додати ДЗ'))
-    await message.answer('Вітаю ви стали частинкою самого гучного класу в школі №6! Чим можу допомогти?',
+    await message.answer('Вітаю ви стали частинкою найгучнішого класу в школі №6! Чим можу допомогти?',
                          reply_markup=markup)
 
 @dp.message_handler(commands=['Розклад'])
